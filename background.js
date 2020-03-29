2063b8bfcc03caa2fcabb6593375f0213672fdfd
Billy Hernandez <hernanbi@ecs.csus.edu>
2020-03-28 22:33:45 -0700

    Session saved to storage

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
 async function getTabs() {
   let tabs = await browser.tabs.query({currentWindow: true});
   var current = browser.windows.getCurrent();
-  console.log("gettabs")
   current.then((response)=>console.log("windowID: " + response.id));
   console.log(tabs);
+  var sessionTabs = tabs[0].url;
+  for (var i = 1; i < tabs.length; i++) {
+    console.log(tabs[i].url);
+    sessionTabs = sessionTabs + ", " + tabs[i].url;
+  }
+  browser.storage.local.set({ session1 : sessionTabs });
 }
 browser.browserAction.onClicked.addListener(() => {
   console.log(getTabs());
