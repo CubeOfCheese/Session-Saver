1949d4875624f772a9f8e62afa3bfefbce32efd9
Billy Hernandez <hernanbi@ecs.csus.edu>
2020-03-28 23:36:42 -0700

    opens new tabs with the saved urls

--- sidebar/panel.js
+++ sidebar/panel.js
@@ -6,12 +6,32 @@ var loadBtn = document.querySelector('button[name="load-sess"]');
 saveNewBtn.addEventListener('click', saveSession);
 loadBtn.addEventListener('click', loadSession);
 
+
 function saveSession() {
   document.body.style.border = "5px solid yellow";
   backgroundPage.saveSession();
 }
+
+function onCreated(tab) {
+  console.log(`Created new tab: ${tab.id}`)
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
 function loadSession() {
   var session1 = browser.storage.local.get("session1");
-  session1.then((response)=>console.log(response.session1.split(", ")));
+  var tabURLs;
+  session1.then((response)=>tabURLs = response.session1.split(", ")).then((response)=>{
+    for (var i = 0; i<tabURLs.length; i++) {
+      console.log(tabURLs[i])
+      var newTab = browser.tabs.create({url:tabURLs[i]});
+      newTab.then(onCreated, onError);
+    }
+  }
+
+  );
   console.log("loadSession")
+
 }
