0f5a0c2d1a1b8e8d7c70a5a29f9e2ee80047ae65
Billy Hernandez <hernanbi@ecs.csus.edu>
2020-04-09 02:12:10 -0700

    Dynamically adds button to load multiple sessions

--- background.js
+++ background.js
@@ -1,10 +1,15 @@
+browser.storage.local.set({ sessionCounter : 1 });
+
 async function saveSession() {
   let tabs = await browser.tabs.query({currentWindow: true});
   var sessionTabs = tabs[0].url;
   for (var i = 1; i < tabs.length; i++) {
     sessionTabs = sessionTabs + ", " + tabs[i].url;
   }
-  browser.storage.local.set({ session1 : sessionTabs });
+  browser.storage.local.set({ session0 : sessionTabs });
+  var numberOfSavedSessions = browser.storage.local.get("sessionCounter");
+  numberOfSavedSessions++;
+  browser.storage.local.set({ sessionCounter : numberOfSavedSessions });
 }
 browser.browserAction.onClicked.addListener(() => {
   saveSession();
--- sidebar/panel.js
+++ sidebar/panel.js
@@ -1,12 +1,29 @@
 let backgroundPage = browser.extension.getBackgroundPage();
+let sessionList = document.querySelector('ul[id="session-list"]');
 var saveNewBtn = document.querySelector('button[name="save-new"]');
 var loadBtn = document.querySelector('button[name="load-sess"]');
 saveNewBtn.addEventListener('click', saveSession);
 loadBtn.addEventListener('click', loadSession);
 
+var numberOfSavedSessions = browser.storage.local.get("sessionCounter");
+numberOfSavedSessions.then((response)=>{
+  console.log(response.sessionCounter);
+  for (var i = 0; i < response.sessionCounter; i++) {
+    console.log("in loop")
+    var session = browser.storage.local.get("session" + i);
+    session.then(()=>{
+      var button = document.createElement("BUTTON");
+      button.innerHTML = "session" + i;
+      button.onclick = loadSession;
+      sessionList.append(button);
+    });
+  }
+})
+
 
 function saveSession() {
   backgroundPage.saveSession();
+
 }
 
 function onCreated(tab) {
@@ -17,10 +34,11 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
+
 function loadSession() {
-  var session1 = browser.storage.local.get("session1");
+  var session0 = browser.storage.local.get("session0");
   var tabURLs;
-  session1.then((response)=>tabURLs = response.session1.split(", ")).then((response)=>{
+  session0.then((response)=>tabURLs = response.session0.split(", ")).then((response)=>{
     for (var i = 0; i<tabURLs.length; i++) {
       console.log(tabURLs[i])
       var newTab = browser.tabs.create({url:tabURLs[i]});
